76dad84e63e56db3e1bc7540935dd4be010532ef
Gen <genrocks88@gmail.com>
2019-08-25 19:10:13 -0400

    git ignore working

---
+++ m0/Tanno_G_m0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public GameObject prefab;
+
+    private Rigidbody rb;
+    private int count;
+    private void Start()
+    {
+        prefab = Resources.Load("projectile") as GameObject;
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed * 3);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+
+            projectile.transform.position = transform.position - Camera.main.transform.forward;
+            //projectile.transform.position = rb.position * Random.Range(-1.0f, 1.0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You Win";
+        }
+    }
+}
---
+++ m0/Tanno_G_m0/Assets/Scripts/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefab = Resources.Load("projectile") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position + Camera.main.transform.forward;
+        }
+    }
+}

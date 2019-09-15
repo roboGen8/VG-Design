80b88de93cdba89e12fcdef275a576cf3a41b7ed
Gen <genrocks88@gmail.com>
2019-09-15 19:41:03 -0400

    2nd hw

---
+++ M2/Assets/Scripts/AppEvents/BeanJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody bean;
+    void Start()
+    {
+        bean = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bean.position.y < 0.2) {
+            bean.AddForce(0, 30 * Random.Range(1,2), 0);
+        }
+        
+    }
+}
